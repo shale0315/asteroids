8ee920fea09221581713ca732618db220a46aafa
shale0315 <jakekubena@pm.me>
2024-10-01 13:31:12 -0500

    added timeclock

--- main.py
+++ main.py
@@ -8,12 +8,16 @@ def main():
     # print(SCREEN_WIDTH)
     # print(SCREEN_HEIGHT)
     running = True
+    timeclock = pygame.time.Clock()
+    dt = 0
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        pygame.display.flip()   
+        pygame.display.flip()
+        dt = timeclock.tick(60) / 1000   
+        
     
 if __name__ == "__main__":
     main()
