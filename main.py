a1e8b5a11789344fe6ff86974601182f868eef7a
shale0315 <jakekubena@pm.me>
2024-10-01 06:03:11 -0500

    Finally set-up

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import pygame
+from constants import *
+
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     print("Starting asteroids!")
+    # print(SCREEN_WIDTH)
+    # print(SCREEN_HEIGHT)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        pygame.display.flip()   
     
 if __name__ == "__main__":
     main()
